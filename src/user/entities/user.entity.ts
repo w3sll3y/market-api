f059344910dbccf27b2b309c6a8c2919a390ace6
Wesley Fernandes <fernandess.weslley@gmail.com>
2024-06-29 11:10:41 -0300

    feat(user): implement create

---
+++ src/user/entities/user.entity.ts
@@ -0,0 +1,13 @@
+export class User {
+  id?: string;
+  email: string;
+  name: string;
+  username: string;
+  password: string;
+  address?: [];
+  photo?: string;
+  receipts?: [];
+  friends?: [];
+  lists?: [];
+  createdat?: string;
+}
---
+++ src/user/user.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { v4 as uuidv4 } from 'uuid';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class UserService {
+  constructor(private readonly prisma: PrismaService) { }
+
+  async create(createUserDto: CreateUserDto) {
+    const data = {
+      id: uuidv4(),
+      ...createUserDto,
+      address: [],
+      password: await bcrypt.hash(createUserDto.password, 10),
+    }
+
+    const userCreated = await this.prisma.user.create({ data });
+
+
+    return {
+      ...userCreated,
+      password: undefined
+    }
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
