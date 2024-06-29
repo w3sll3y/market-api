bf209b30a4a553ec0f49608c2983468a93444135
Wesley Fernandes <fernandess.weslley@gmail.com>
2024-06-29 11:13:09 -0300

    feat(user): implement findByEmail

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -26,8 +26,10 @@ export class UserService {
     }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  findByEmail(email: string) {
+    return this.prisma.user.findUnique({
+      where: { email },
+    });
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
